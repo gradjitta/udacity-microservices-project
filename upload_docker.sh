b070925286aa61569d44cf218110b46d52b6675c
pmbrull <peremiquelbrull@gmail.com>
2019-07-31 18:32:30 +0200

    parametrized upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,29 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
+# Step 0:
+# Load configurations
+. ./cfg.sh
+
+# Docker image parameters
+path=udacity_devops_api
+version=0.1
+
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=$username/$path;
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+# Get imageId from one tag, e.g., latest
+imageId=$(docker images -q $dockerpath:latest)
+
+docker login -u=$username -p=$docker_pwd
+docker tag $imageId $dockerpath:$version
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
